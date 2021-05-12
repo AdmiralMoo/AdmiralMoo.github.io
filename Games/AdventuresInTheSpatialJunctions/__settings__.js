50f42339de530be31c2d5863bdc14b04f22d7e46
AdmiralMoo <40005789+AdmiralMoo@users.noreply.github.com>
2021-05-12 00:16:13 -0400

    Added Adventures In The Spatial Junctions

---
+++ Games/AdventuresInTheSpatialJunctions/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1017733.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 36437552, 36592481, 36659076, 36888366, 36895794, 36895795, 36944892, 36998954, 37064417, 37185184, 37491709, 37556652, 37558036, 37788924, 37798580, 37798582, 37798834 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/36801160/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/36801161/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/36801159/1/ammo.js', 'preload' : true},
+];
